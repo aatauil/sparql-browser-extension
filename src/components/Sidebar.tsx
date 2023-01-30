14566da79d5070d2b920fc639850b7e55131263f
Achraf Atauil <achrafatauil@hotmail.com>
2023-01-30 18:36:06 +0100

    sidebar features (see description)
    - add SbFile component showing and handling query list functionality
    - add workspace logic and styling (will move to own component)
    - add favorites list

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,55 +1,112 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from '~data/db';
+import { useState } from 'react';
+import SbFiles from './Sidebar/SbFiles';
+import useOnClickOutside from '~hooks/useOnClickOutside';
 
 function Sidebar() {
+  const wsInput = useRef()
 
-  const files = useLiveQuery(() => db.files.toArray());
+  const [isCreatingWs, setIsCreatingWs] = useState(false);
+  const [wsName, setWsName] = useState("");
+
+  const focusedWorkspace = useLiveQuery(() => db.workspaces.where({ focused: 1 }).first());
+  const workspaces = useLiveQuery(() => db.workspaces.toArray());
+  const favorites = useLiveQuery(() => db.files.where({favorite: 1}).toArray());
+
+  useOnClickOutside(wsInput, () => {
+    setIsCreatingWs(false)
+  })
   
-  async function addFile() {
-    const now = new Date().toISOString();
-    db.files.where('focused').equals(1).modify({ focused: 0 });
-    db.files.add({
-      name: now,
-      code: 'SELECT * WHERE { ?s ?p ?o } LIMIT 10',
-      focused: 1,
-      folderId: -1,
-      created: now,
-      modified: now
-    });
-  }
+  async function addWorkspace() {
+    const now = new Date()
 
-  function deleteFile(fileId) {
-    db.files.delete(fileId);
-  }
-
-  function setFile(fileId) {
+    db.workspaces.where('focused').equals(1).modify({focused: 0});
     db.files.where('focused').equals(1).modify({focused: 0});
-    db.files.update(fileId, { focused: 1 });
+
+    db.workspaces.add({
+      name: wsName,
+      focused: 1,
+      created: now
+    });
+    
   }
 
-  function isFocused(value) {
-    if(value) {
-      return "text-black"
-    } else {
-      return "hover:bg-zinc-100";
-    }
+  function deleteFavorite(file) {
+    db.files.update(file, {
+      favorite: -1
+    })
   }
+
+  function setWorkspace(workspace) {
+    db.workspaces.where('focused').equals(1).modify({focused: 0});
+    db.files.where('focused').equals(1).modify({focused: 0});
+
+    db.workspaces.update(workspace, { focused: 1 });
+  }
+  
+  const handleKeyDown = (event) => {
+    if (event.key === 'Escape') {
+      setIsCreatingWs(false)
+      setWsName("")
+    }
+    if (event.key === 'Enter') {
+      addWorkspace()
+      setIsCreatingWs(false)
+      setWsName("")
+    }
+  };
 
   return (
-    <div className='flex items-center w-full space-x-3'>
-      <div className='flex flex-col text-gray-600 h-full w-full'>    
-        <button className='bg-zinc-100 px-2 py-1 text-gray-800 rounded hover:bg-zinc-300 ml-2 mb-2 border-b' onClick={addFile}>
-          <i className="ri-add-line"></i>
-        </button>
-        {files?.map((file, index) => (
-          <div key={index} className={`flex items-center justify-between p-4 cursor-pointer text-base border-b ${isFocused(file?.focused)}`} >
-            <div onClick={() => setFile(file.id)}>{file.name}</div>
-            <button className='hover:text-red-500 rounded-full text-gray-700 flex items-center justify-center h-5 w-5 ' onClick={() => deleteFile(file.id)}>
-              <i className="ri-close-line "></i>
-            </button> 
-          </div> 
-        ))}
+    <div className='flex flex-col text-gray-600 h-full w-60 border-r border-zinc-300'> 
+      <div className='flex items-center space-x-2 bg-zinc-100 p-3 text-sm'>
+        <i className="ri-bubble-chart-fill text-blue-600"></i>
+        <h1 className='font-medium'>Sparql browser extention</h1>
+      </div> 
+      <div className='py-4'>
+        <div className='flex items-center justify-between px-3 mb-2'>
+          <h2 className='text-sm font-medium text-zinc-900'>Workspaces</h2> 
+          <button className='bg-blue-600 px-1 py-.5 text-white text-md rounded hover:bg-blue-500' onClick={() => setIsCreatingWs(true)}>
+            <i className="ri-add-line text-sm"></i>
+          </button>
+        </div>
+
+        <div className='p-2 space-y-1'>
+          {isCreatingWs ?
+            <div ref={wsInput} className='flex items-center space-x-1 cursor-pointer border text-xs p-1 rounded font-medium'>
+              <i className="ri-layout-2-line text-base"></i>
+              <input autoFocus type="text" value={wsName} onChange={(e) => setWsName(e.target.value)} onKeyDown={handleKeyDown} className='w-full p-1 pl-0 rounded border-none focus:ring-0 text-xs' placeholder='Workspace name'/>
+            </div>
+            : 
+            ""
+          }
+
+          {workspaces?.map((ws, index) => (
+            <div key={index} onClick={() => setWorkspace(ws)} className={`flex items-center space-x-1 cursor-pointer border text-xs p-1 rounded font-medium ${ws.focused ? "bg-blue-100 text-black border-2 border-blue-400" : "hover:bg-zinc-100"}`} >
+              <i className="ri-layout-2-line text-base"></i>
+              <div className='flex-1'>{ws.name}</div>
+              <button className='hover:text-red-500 rounded-full text-gray-700 flex items-center justify-center h-5 w-5 '>
+                <i className="ri-close-line "></i>
+              </button> 
+            </div> 
+          ))}
+        </div>
+      </div>
+      <SbFiles />
+      <div className='border-t py-4'>
+        <h2 className='px-3 text-sm font-medium text-zinc-900'>Favorite</h2> 
+        <div className='p-2 space-y-1'>
+          {favorites?.map((file, index) => (
+              <div key={index} className={`flex items-center space-x-1 cursor-pointer text-xs p-1 rounded`} >
+                <i className="ri-file-list-2-line text-base"></i>
+                <div className='flex-1' onClick={() => setFile(file.id)}>{file.name}</div>
+                <button className='hover:text-red-500 rounded-full text-gray-700 flex items-center justify-center h-5 w-5 ' onClick={() => deleteFavorite(file)}>
+                  <i className="ri-close-line "></i>
+                </button> 
+              </div> 
+            ))}
+        </div>
       </div>
     </div>
   )
---
+++ src/components/Sidebar/SbFiles.tsx
@@ -0,0 +1,107 @@
+import React, { useRef, useState } from 'react';
+import { useLiveQuery } from "dexie-react-hooks";
+import { db } from '~data/db';
+import useOnClickOutside from '~hooks/useOnClickOutside';
+
+function SbFiles() {
+  const fileInput = useRef()
+
+  const [isCreating, setIsCreating] = useState(false);
+  const [fileName, setFileName] = useState();
+
+  const workspace = useLiveQuery(() => db.workspaces.where({ focused: 1 }).first());
+  const files = useLiveQuery(() => db.files.where({ workspaceId: workspace?.id || -1 }).toArray(), [workspace]);
+
+  useOnClickOutside(fileInput, () => {
+    setIsCreating(false)
+  })
+
+  if(!workspace) return null;
+
+  async function addFile() {
+    const now = new Date()
+
+    const humanDate = now.toLocaleString("en-GB", {
+      month: "long",
+      day: "numeric",
+    });
+
+    const name = fileName?.length ? fileName : `untitled query - ${humanDate}`;
+
+    db.files.where('focused').equals(1).modify({ focused: 0 });
+    db.files.add({
+      name: name,
+      code: `SELECT * WHERE {
+  ?s ?p ?o 
+} LIMIT 10`,
+      focused: 1,
+      favorite: 0,
+      created: now,
+      workspaceId: workspace.id,
+      modified: now
+    });
+  }
+
+  function deleteFile(fileId) {
+    db.files.delete(fileId);
+  }
+
+  function setFile(fileId) {
+    db.files.where('focused').equals(1).modify({focused: 0});
+    db.files.update(fileId, { focused: 1 });
+  }
+
+  function isFocused(value) {
+    if(value) {
+      return "bg-blue-200 text-black"
+    } else {
+      return "hover:bg-zinc-100";
+    }
+  } 
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Escape') {
+      setIsCreating(false)
+      setFileName("")
+    }
+    if (event.key === 'Enter') {
+      addFile()
+      setIsCreating(false)
+      setFileName("")
+    }
+  };
+  
+  return (
+    <div className='border-t py-4'>
+      <div className='flex items-center justify-between px-3'>
+        <h2 className='text-sm font-medium text-zinc-900'>Queries</h2> 
+        <button className='bg-blue-600 px-1 py-.5 text-white text-md rounded hover:bg-blue-500'onClick={() => setIsCreating(true)}>
+          <i className="ri-add-line text-sm"></i>
+        </button>
+      </div>
+      <div className='relative px-3 pt-2 w-full'>
+        <i className="ri-search-line absolute top-3 left-5"></i>
+        <input type="search" name="" id="" className='bg-zinc-100 w-full p-1 pl-7 rounded text-xs' placeholder='Search...'/>
+      </div>
+      <div className='p-2 space-y-1'>
+        {isCreating &&
+          <div ref={fileInput} className={`flex items-center space-x-1 cursor-pointer text-xs p-1 rounded`} >
+            <i className="ri-file-list-2-line text-base"></i>
+            <input autoFocus type="text" value={fileName} onChange={(e) => setFileName(e.target.value)} onKeyDown={handleKeyDown} className='w-full p-1 pl-0 rounded border-none focus:ring-0 text-xs' placeholder='Untitled query'/>
+          </div> 
+        }
+        {files?.map((file, index) => (
+          <div key={index} onClick={() => setFile(file.id)} className={`flex items-center space-x-1 cursor-pointer text-xs p-1 rounded ${file.focused ? "bg-blue-200 text-black" : "hover:bg-zinc-100"}`} >
+            <i className="ri-file-list-2-line text-base"></i>
+            <div className='flex-1' >{file.name}</div>
+            <button className='hover:text-red-500 rounded-full text-gray-700 flex items-center justify-center h-5 w-5 ' onClick={() => deleteFile(file.id)}>
+              <i className="ri-close-line "></i>
+            </button> 
+          </div> 
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SbFiles
