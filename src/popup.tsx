19f6b838e37f6f9a6b99f3b09ea600e5fe4043b7
Achraf Atauil <achrafatauil@hotmail.com>
2023-01-31 12:40:10 +0100

    add link to editor in popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,20 @@
 import "~style.css"
 
 function IndexPopup() {
+  function createTab() {
+    chrome.tabs.create({
+      url: './tabs/index.html'
+    })
+  }
   return (
-    <div className="flex items-center justify-center h-16 w-40">
-
+    <div className="border w-64 p-2">
+      <div className='flex items-center space-x-2 p-2 text-sm'>
+        <i className="ri-bubble-chart-fill text-blue-600"></i>
+        <h1 className='font-extrabold text-zinc-800'>Sparql browser extention</h1>
+      </div> 
+      <div className="w-full">
+        <button onClick={() => createTab()} className="w-full p-2 text-center font-medium bg-blue-600 text-white rounded hover:bg-blue-700">open editor</button>
+      </div>
     </div>
   )
 }
