f615fd57cbaf9b528abdc3a7d78f7d8c0cb63733
Achraf Atauil <achrafatauil@hotmail.com>
2023-03-14 17:10:40 +0100

    update db (duration, error, isLoading)

--- src/data/db.ts
+++ src/data/db.ts
@@ -4,7 +4,7 @@ export const db = new Dexie('myDatabase');
 
 db.version(1).stores({
   workspaces: '++id, name, focused, created',
-  files: '++id, name, workspaceId, focused, code, output, created, modified, favorite',
+  files: '++id, name, workspaceId, focused, code, output, created, modified, favorite, duration, error, isLoading',
   endpoints: '++id, value, focused'
 })
 
