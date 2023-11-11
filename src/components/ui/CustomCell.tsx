6f2fa28c8c8a2158092d0ea23512bf71b88b634a
Achraf Atauil <achrafatauil@hotmail.com>
2023-11-11 18:23:30 +0100

    add cases for when no value or no shortened value is given

--- src/components/ui/CustomCell.tsx
+++ src/components/ui/CustomCell.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
 
 function CustomCell({value}) {
-  const [isToggled, setToggle] = useState(!!value.shortened)
+  const [isToggled, setToggle] = useState(!!value?.shortened)
+
+  if(!value) {
+    return <div></div>
+  }
+
+  if(!value.shortened) {
+    return <div>{value.uri}</div>
+  }
+  
   return (
-    <div onDoubleClick={() => setToggle(!isToggled)}>
+    <div onDoubleClick={() => setToggle(!isToggled)} className={isToggled && "font-medium"}>
       {isToggled ? value.shortened : value.uri}
     </div>
   )
